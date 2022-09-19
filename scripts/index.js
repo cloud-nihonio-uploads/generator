6440b188ac636beaacc408c95f3bb67d83228aa5
nihonio-cloud <110445763+cloud-nihonio-uploads@users.noreply.github.com>
2022-09-18 22:04:14 -0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
+function gen() => {
 window.alert('hi')
 btn = document.getElementById('generate')
 btc = document.getElementById('final')
 
-btn.onclick => function() {
-  btc.innerText = "test"
+btc.innerText = "test"
 }
